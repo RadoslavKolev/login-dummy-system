5942b183a105254a7ad2d8d347508d8fac2b4860
rkolev <radoslav.kolev@dxc.com>
2023-01-11 13:41:45 +0200

    Preventing Logout when refreshing the page

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import Login from './components/Login/Login';
 import Home from './components/Home/Home';
@@ -7,13 +7,29 @@ import MainHeader from './components/MainHeader/MainHeader';
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
+  // useEffect guarantees that this function will execute only when the dependencies are changed
+  // It will execute when the application first loads
+  useEffect(() => {
+    const storedUserInformation = localStorage.getItem('isLoggedIn');
+
+    if (storedUserInformation === '1') {
+      setIsLoggedIn(true);
+    }
+  }, []);
+
   const loginHandler = (email, password) => {
     // We should of course check email and password
     // But it's just a dummy/ demo anyways
+
+    // localStorage is global object available in the browser
+    // In setItem() we give an identifier and what do we store in the browser storage
+    // 1 is for Logged In, 0 is for Logged Out
+    localStorage.setItem('isLoggedIn', '1');
     setIsLoggedIn(true);
   };
 
   const logoutHandler = () => {
+    localStorage.removeItem('isLoggedIn');
     setIsLoggedIn(false);
   };
 
